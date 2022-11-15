48a2ff8d03cdf7fff8b777077409dcbd7d8b6d1e
Ilya <b.ilya@mail.ru>
2022-11-15 15:34:34 +0300

    Выполнил дз по Maven и Spring Boot + web

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,58 @@
+package com.example.employee.controller;
+
+import com.example.employee.model.Employee;
+import com.example.employee.record.EmployeeRequest;
+import com.example.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.OptionalInt;
+
+@RestController
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return this.employeeService.addEmployee(employeeRequest);
+    }
+
+    @GetMapping("employees/salary/sum")
+    public int getSalarySum() {
+        return this.employeeService.getSalarySum();
+    }
+
+    @GetMapping("employees/salary/min")
+    public OptionalInt getMinSalaryByEmployees() {
+        return this.employeeService.getMinSalaryByEmployees();
+
+    }
+
+    @GetMapping("employees/salary/max")
+    public OptionalInt getMaxSalaryByEmployees() {
+        return this.employeeService.getMaxSalaryByEmployees();
+    }
+
+    @GetMapping("employees/salary/average/more")
+    public Collection<Employee> getAllEmployeesSalaryMoreAverage() {
+        return this.employeeService.getAllEmployeesSalaryMoreAverage();
+    }
+
+    @GetMapping("employees/salary/average/less")
+    public Collection<Employee> getAllEmployeesSalaryLessAverage() {
+        return this.employeeService.getAllEmployeesSalaryLessAverage();
+    }
+}
---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,71 @@
+package com.example.employee.service;
+
+import com.example.employee.model.Employee;
+import com.example.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class EmployeeService {
+
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("У сотрудника не заполнены поля имени");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(),
+                employeeRequest.getFirstName(),
+                employeeRequest.getDepartment(),
+                employeeRequest.getSalary());
+
+        employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public OptionalInt getMinSalaryByEmployees() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .min();
+    }
+
+    public OptionalInt getMaxSalaryByEmployees() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .max();
+    }
+
+    public Collection<Employee> getAllEmployeesSalaryMoreAverage() {
+        int averageSalary = (int) employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .average()
+                .orElse(0);
+
+        return employees.values().stream()
+                .filter(e -> e.getSalary() > averageSalary)
+                .toList();
+    }
+
+    public Collection<Employee> getAllEmployeesSalaryLessAverage() {
+        int averageSalary =(int) employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .average()
+                .orElse(0);
+
+        return employees.values().stream()
+                .filter(e -> e.getSalary() < averageSalary)
+                .toList();
+    }
+}
